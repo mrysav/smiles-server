f45e87e9059fa6ec23c5fc078d95afa77adc3eff
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2018-03-08 20:22:13 -0500

    add message css classes, use bootstrap for layout

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,9 @@
 import * as io from "socket.io-client";
 import * as $ from "jquery";
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const socket = io('/');
 
 console.log('connecting...');
@@ -9,12 +12,26 @@ socket.on('connect', function() {
     console.log('connected');
 });
 
-$('form').submit(function () {
-    socket.emit('sms', $('#m').val());
-    $('#m').val('');
-    return false;
-});
-
 socket.on('sms', function(msg) {
     console.log(msg);
 });
+
+$('form').submit(function () {
+    var text = $('#m').val().toString();
+    new_message(text);
+    $('#m').val('');
+    var elem = document.getElementById('messages');
+    elem.scrollTop = elem.scrollHeight;
+    return false;
+});
+
+function new_message(text:String) {
+    if (text === "null" || text === "") {
+        return;
+    }
+    socket.emit('sms', text);
+    var newmsg = $('<div />');
+    newmsg.addClass('message self');
+    newmsg.text(text.toString());
+    $('#messages').append(newmsg);
+}
